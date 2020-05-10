1167caa0e626aa0088a58aeecd3c1351bb096be7
AlexxSaz <alexxsaz96@gmail.com>
2020-05-11 00:43:14 +0700

    Изменено название класса по RSDN и добавлено поле названия фигуры

---
+++ oop_lab3/ShapeLib/ShapeBase.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Mvvm;
+
+namespace ShapeLib
+{
+    //TODO: RSDN - название класса (V)
+    /// <summary>
+    /// Геометрические фигуры
+    /// </summary>
+    public abstract class ShapeBase : BindableBase
+    {
+        private string _nameOfShape;
+
+        public string NameOfShape
+        {
+            get => _nameOfShape;
+            set
+            {
+                _nameOfShape = value;
+            }
+        }
+
+        //TODO: Не имеет смысла virtual (V)
+        /// <summary>
+        /// Расчет объема фигуры
+        /// </summary>
+        public abstract double Volume { get; }
+
+        /// <summary>
+        /// Проверка правильности введенных данных
+        /// </summary>
+        /// <param name="data">Введенные данные</param>
+        protected static void CheckData(double data)
+        {
+            if (double.IsNaN(data))
+            {
+                throw new ArgumentException(
+                    string.Format("***Это не число***\nВведите число!",
+                    nameof(data)));
+            }
+            else if (data < 0)
+            {
+                throw new ArgumentException(
+                    string.Format("***Это число отрицаетльное***\n" +
+                    "Введите число не меньше нуля!", nameof(data)));
+            }
+        }
+    }
+}
