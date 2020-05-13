139a9c3114017c6813b64653b32a00950e80dc3a
AlexxSaz <alexxsaz96@gmail.com>
2020-05-13 11:11:28 +0700

    Добавлено свойство объем и реализован экзекут этого свойства

--- oop_lab3/oop_lab3/ViewModel/ShapesViewModel.cs
+++ oop_lab3/oop_lab3/ViewModel/ShapesViewModel.cs
@@ -3,41 +3,52 @@ using System.Collections.Generic;
 using System.Text;
 using System.Windows;
 using System.ComponentModel;
-
 using ShapeLib;
 using System.Runtime.CompilerServices;
 using System.Collections.ObjectModel;
+using Prism.Mvvm;
+using Prism.Commands;
+using Prism.Common;
+using System.Windows.Input;
 
 namespace oop_lab3.ViewModel
 {
-    public class ShapesViewModel : INotifyPropertyChanged
+    public class ShapesViewModel : BindableBase
     {
         public ObservableCollection<ShapeBase> Shapes { get; set; }
-        private ShapeBase _shapeBase;
+        private ShapeBase _selectedShape;
+        private double _volume;
         public ShapeBase SelectedShape
         {
-            get => _shapeBase;
+            get => _selectedShape;
             set
             {
-                _shapeBase = value;
-                OnPropertyChanged(nameof(SelectedShape));
+                _selectedShape = value;
+                RaisePropertyChanged(nameof(SelectedShape));
             }
         }
 
+        public double Volume { get; set; }
+
         public ShapesViewModel()
         {
+            SelectedShape = new Parallelepiped();
+            SelectedShape.PropertyChanged += (s, e) => { RaisePropertyChanged(e.PropertyName); };
             Shapes = new ObservableCollection<ShapeBase>
             {
-                new Parallelepiped {NameOfShape=nameof(Parallelepiped), Height=0, SquareOfBase=0},
-                new Sphere {NameOfShape=nameof(Sphere), Radius=0},
-                new Pyramid {NameOfShape=nameof(Pyramid), Height=0, SquareOfBase=0}
+                new Parallelepiped {NameOfShape=nameof(Parallelepiped)},
+                new Sphere {NameOfShape=nameof(Sphere)},
+                new Pyramid {NameOfShape=nameof(Pyramid)}
             };
+            CalcVolume = new DelegateCommand(() => { ExecuteCalc(); }).ObservesProperty(() => Volume);
         }
 
-        public event PropertyChangedEventHandler PropertyChanged;
-        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        private void ExecuteCalc()
         {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+            Volume = SelectedShape.Volume;
+            RaisePropertyChanged(nameof(Volume));
         }
+
+        public DelegateCommand CalcVolume { get; }
     }
 }
